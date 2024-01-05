72283805fa3848dfbc017aa7e01514580c71ac9d
Jhon Fa <jhonfaverm@gmail.com>
2024-01-05 12:17:13 -0500

    add port

--- index.js
+++ index.js
@@ -42,15 +42,17 @@ app.get('/', (req, res) => {
     res.send('¡Hola, esta es la página de inicio de mi aplicación!');
   });
 
+
+ /* 
 app.listen(config.port, () => {
     console.log("Server listening on port ", config.port);
 });
+*/
 
-/*  con este lo tenia funcionando
 app.listen(port, () => {
     console.log("Server listening on port " + process.env.PORT);
 });
-*/
+
 
 
 /* 
